0b28fedc5b8e37033c62eca7a2d531331e343ba2
Frank1963-mpoyi <frankmpoyi63@gmail.com>
2020-12-30 20:44:58 +0200

    front end work

---
+++ food/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from .models import Pizza, Burger
+
+
+
+def index (request):
+    template_name ='food/index.html'
+    
+    context={'active_link':'index'}
+    return render (request, template_name, context)
+
+def burger (request):
+    template_name ='food/burger.html'
+    queryset = Burger.objects.all()
+    context={
+        'queryset': queryset,
+        'active_link':'burger'
+        }
+    return render (request, template_name, context)
+
+
+def pizza (request):
+    template_name = 'food/pizza.html'
+    queryset= Pizza.objects.all()
+    context= {
+        'queryset': queryset,
+        'active_link':'pizza'
+        }
+    return render (request, template_name, context)
