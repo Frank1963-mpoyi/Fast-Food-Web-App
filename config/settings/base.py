6347a6f846932edaaabd7b71c6b6fc5a27d1db51
Frank1963-mpoyi <frankmpoyi63@gmail.com>
2021-01-01 21:17:45 +0200

    debug True

--- config/settings/base.py
+++ config/settings/base.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'ruthmitongorestaurant.herokuapp.com']
 
