757dcaa10913a8eb4e2f177592a1d6c10ef65dae
Frank1963-mpoyi <frankmpoyi63@gmail.com>
2021-01-02 15:57:09 +0200

    deleted

--- .env_fastFood/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\mpoyi tshibuyi\desktop\fast-food-web-app\.env_fastfood\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
