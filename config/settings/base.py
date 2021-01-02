de2a2e8465f33cf044db0303e890c1c31f7cb711
Frank1963-mpoyi <frankmpoyi63@gmail.com>
2021-01-02 16:10:42 +0200

    n

--- config/settings/base.py
+++ config/settings/base.py
@@ -24,7 +24,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'ruthmitongorestaurant.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'mitonguruth.herokuapp.com']
 
 
 # Application definition
